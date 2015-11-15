40ad2391368d5bee1a00cb2c98e4c6b49b46bc30
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-15 23:03:50 +1030

    Kinda working.

--- bitbang/bitbang.py
+++ bitbang/bitbang.py
@@ -10,6 +10,12 @@ class BitBang(object):
 	def setto(self, value_name):
 		raise NotImplementedError
 
+	def bit_to_carry(self):
+		raise NotImplementedError
+
+	def carry_to_bit(self):
+		raise NotImplementedError
+
 	def get(self):
 		raise NotImplementedError
 		
@@ -24,6 +30,9 @@ class BitBang(object):
 		
 	def shift_out(self):
 		raise NotImplementedError
+
+def indent(s):
+	return ("\n	".join(s.split("\n")))
 
 
 class ByteAccessInC(BitBang):
@@ -44,7 +53,6 @@ class ByteAccessInC(BitBang):
 		self.name_nask_def = name.upper() + "_NASK"
 
 	def defines(self):
-		print(bin(self.nask))
 		return """\
 #define %(name_port_def)s %(portname)s
 #define %(name_mask_def)s %(mask)#04X
@@ -54,17 +62,31 @@ class ByteAccessInC(BitBang):
 	def set(self):
 		return """\
 %(name_port_def)s |= %(name_mask_def)s; /* Set %(name)s */""" % self.__dict__
-
-	def setto(self, value_name):
-		d = {}
-		d.update(self.__dict__)
-		d['value_name'] = value_name
-		return """\
-%(name_port_def)s |= (%(name_mask_def)s & %(value_name)s); /* Set %(name)s */""" % d
-
+	
 	def get(self):
 		return """\
 (%(name_port_def)s & %(name_mask_def)s) /* Get %(name)s */""" % self.__dict__
+
+	def setto(self, value_name):
+		return """\
+if (%(value_name)s) {
+	%(set)s
+} else {
+ 	%(clear)s
+}""" % {
+	'value_name': value_name,
+	'set': self.set(),
+	'clear': self.clear(),
+	}
+
+	def carry_to_bit(self):
+		return """\
+__asm__ ("");
+"""
+	def bit_to_carry(self):
+		return """\
+__asm__ ("");
+"""
 
 	def clear(self):
 		return """\
@@ -111,6 +133,14 @@ class BitAccessInC(BitBang):
 		return """\
 %(name_portbit_def)s /* Get %(name)s */""" % self.__dict__
 
+	def bit_to_carry(self):
+		return """\
+__asm__ ("mov c,%(name_portbit_def)s"); /* %(name)s->carry */""" % self.__dict__
+
+	def carry_to_bit(self):
+		return """\
+__asm__ ("mov %(name_portbit_def)s,c"); /* carry->%(name)s */""" % self.__dict__
+
 	def clear(self):
 		return """\
 %(name_portbit_def)s = 0; /* Clear %(name)s */""" % self.__dict__
@@ -131,11 +161,11 @@ inline void %(name)s_set() {
 	%(set)s;
 }
 
-inline void %(name)s_setto(bool value) {
-	%(setto)s;
+inline void %(name)s_setto(_Bool value) {
+	%(setto)s
 }
 
-inline bool %(name)s_get() {
+inline _Bool %(name)s_get() {
 	return %(get)s;
 }
 
@@ -152,30 +182,224 @@ inline void %(name)s_toggle() {
 	'class': bitbang.__class__,
 	'name': bitbang.name,
 	'defines': bitbang.defines(),
-	'set': bitbang.set(),
-	'setto': bitbang.setto("value"),
+	'set': indent(bitbang.set()),
+	'setto': indent(bitbang.setto("value")),
 	'get': bitbang.get(),
-	'clear': bitbang.clear(),
-	'toggle': bitbang.toggle(),
+	'clear': indent(bitbang.clear()),
+	'toggle': indent(bitbang.toggle()),
 	}
 
 
+
+print("""
+
+#include "fx2regs.h"
+
+#define ROTATE_LEFT(i, n) \
+        i = ((i >> (8-n)) | (i << n))
+
+#define ROTATE_RIGHT(i, n) \
+        i = ((i >> n) | (i << (8-n)))
+
+
+""")
+
 porta_byte = ByteAccessInC("a1", "A", 0)
-porta_bit = BitAccessInC("a1", "A", 0)
+porta_bit = BitAccessInC("a2", "A", 0)
 
 print(GenerateFunctions(porta_byte))
 print(GenerateFunctions(porta_bit))
+print("""
+
+int main() {
+	BYTE b1 = 0;
+	BYTE b2 = 0;
+
+	a1_set();
+	a1_clear();
+	a1_toggle();
+
+	a2_set();
+	a2_clear();
+	a2_toggle();
+
+	b1 |= a1_get();
+	ROTATE_LEFT(b1, 1);
+	b1 |= a1_get();
+	ROTATE_LEFT(b1, 1);
+
+	b2 |= a2_get();
+	ROTATE_LEFT(b2, 1);
+	b2 |= a2_get();
+	ROTATE_LEFT(b2, 1);
+
+	return b1 + b2;
+}
+
+""")
+
+
+from enum import Enum
+
+class ClockMode(Enum):
+	none = 0
+	positive = 1
+	negative = 2
+
+def ShiftByte(clock, data_out, data_in, read_on, write_on):
+	"""
+	ShiftByte(ClockMode.positive, ClockMode.negative)
+	"""
+
+	cmds = []
+
+	if write_on == ClockMode.negative:
+		cmds.append("rotate carry /* data->carry %i */" % 0)
+
+	for i in range(0, 8):
+		cmds.append("/* Bit %i */" % i)
+
+		# Negative edge
+		cmds.append("\_")
+
+		if write_on == ClockMode.negative:
+			cmds.append("write")
+		elif write_on == ClockMode.positive:
+			cmds.append("rotate carry /* data->carry %i */" % i)
+
+		if read_on == ClockMode.negative:
+			cmds.append("read")
+		elif read_on == ClockMode.positive:
+			cmds.append("rotate carry /* carry->data %i */" % i)
+
+		# Positive edge
+		cmds.append("_/")
+
+		if write_on == ClockMode.positive:
+			cmds.append("write")
+		elif write_on == ClockMode.negative:
+			cmds.append("rotate carry /* data->data %i */" % i)
+
+		if read_on == ClockMode.positive:
+			cmds.append("read")
+		elif read_on == ClockMode.negative:
+			cmds.append("rotate carry /* carry->data %i */" % i)
+
+
+	cmds.append("/* After */")
+	if read_on == ClockMode.positive:
+		cmds.append("rotate carry /* carry->data %i */" % i)
+
+	print(cmds)
+
+
+#	rotate carry /* carry->data */
+#	rotate carry /* data->carry */
+# --
+#	rotate carry /* carry->data->carry */
+
+#(a)
+#	rotate carry /* carry->data */
+#	(....)
+#	rotate carry /* data->carry */
+# --
+#	rotate carry /* carry->data->carry */
+#	\1
+
+#(b)
+#	rotate carry /* carry->data */
+#	(....)
+#	rotate carry /* data->carry */
+# --
+#	\1
+#	rotate carry /* carry->data->carry */
+
+	i = 0
+	while i < len(cmds)-1:
+		if cmds[i].startswith("rotate carry") and cmds[i+1].startswith("rotate carry"):
+			cmds.pop(i)
+			cmds.pop(i)
+			cmds.insert(i, "rotate carry /* carry->data->carry */")
+		i += 1
+	
+	print(cmds)
+
+	output = []
+	for cmd in cmds:
+		if cmd == "\_":
+			output.append(clock.clear())
+		elif cmd == "_/":
+			output.append(clock.set())
+		elif cmd == "read":
+			output.append(data_in.bit_to_carry())
+		elif cmd == "write":
+			output.append(data_out.carry_to_bit())
+		elif cmd == "nops":
+			output.append("DELAY;")
+		elif cmd.startswith("rotate carry"):
+			output.append("""\
+__asm__ ("rlc"); %s""" % cmd[13:])
+		elif cmd.startswith("/*"):
+			output.append(cmd)
+
+	defs = ''
+	args = ''
+	if read_on != ClockMode.none:
+		if write_on != ClockMode.none:
+			args = 'BYTE data'
+		else:
+			defs = 'BYTE data = 0;'
+
+		rettype = 'BYTE'
+		ret = 'return data;'
+	else:
+		rettype = 'void'
+		ret = 'return;'
+
+	print("""\
+/* ---------------------------- */
+%(rettype)s shift_byte(%(args)s) {
+	%(defs)s""" % locals())
+	print("	"+"\n	".join(output))
+	print("""\
+	%(ret)s
+}
+/* ---------------------------- */""" % locals())
+
+clock = BitAccessInC("clk", "E", 5)
+data_out = BitAccessInC("dout", "E", 2)
+data_in = BitAccessInC("din", "E", 3)
+
+print("// read:none, write:none")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.none, write_on=ClockMode.none)
+print("// ---------\n")
+print("// read: +ve, write:none")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.positive, write_on=ClockMode.none)
+print("// ---------\n")
+print("// read: -ve, write:none")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.negative, write_on=ClockMode.none)
+print("// ---------\n")
+print("// read: +ve, write: +ve")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.positive, write_on=ClockMode.positive)
+print("// ---------\n")
+print("// read: +ve, write: -ve")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.positive, write_on=ClockMode.negative)
+print("// ---------\n")
+print("// read: -ve, write: +ve")
+ShiftByte(clock, data_out, data_in, read_on=ClockMode.negative, write_on=ClockMode.positive)
+print("// ---------\n")
+
 
 def ShiftByte(bitbang):
 	return """
-
-
 inline BYTE %(name)s_shift(BYTE input) {
+	_Bool carry = 0;
 	BYTE output = 0;
 
-	output = %(name)s_get();
-	
+	carry = input & 0x1;
+	ROTATE_RIGHT(input, 1);
+	%(name)s_setto(carry);
+	output &= %(name)s_get();
+	ROTATE_LEFT(output, 1);
 }
-
-
 """
